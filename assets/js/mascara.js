89342e2c802832ca92b5383c9f027b57725dddf3
qu4resm4 <quaresmacont@gmail.com>
2023-10-29 08:59:25 -0300

    mascara de campos
    algoritmo implementado cheio de bug
    
    melhorar a lógica de implementação de máscaras nos campos

--- assets/js/mascara.js
+++ assets/js/mascara.js
@@ -2,17 +2,57 @@ const cpfInput = document.querySelector("#cpf");
 const celInput = document.querySelector("#tel-cel");
 const fixInput = document.querySelector("#tel-fix");
 
-celInput.addEventListener("input", (event) => {
-    //
-    var valorLimpo = celInput.value.replace(/\D/g, "").substring(0, 11);
 
-    var numerosArray = valorLimpo.split("");
-
-    var numeroFormatado = ""
-
-    if(numerosArray.length > 0){
-        numeroFormatado += `(${numerosArray.slice(0,2).join("")})`;
+function maskPhone(input) {
+        var valorLimpo = input.value.replace(/\D/g, "").substring(0, 10);
+        var numerosArray = valorLimpo.split("");
+        var numeroFormatado = ""
+        //PROBLEMA: ddd não apaga 
+        if(numerosArray.length === 1){
+            numeroFormatado += `(${numerosArray.slice(0,2).join("")})`;
+        };
+        if(numerosArray.length > 2){
+            numeroFormatado += `${numerosArray.slice(2,6).join("")}`;
+        };
+        if(numerosArray.length > 6){
+            numeroFormatado += `-${numerosArray.slice(6,10).join("")}`
+        };
+    
+        input.value = numeroFormatado;
     };
     
-    celInput.value = numeroFormatado;
-});
+    function maskCPF(input){
+        //bugado
+        // 111.111.111-00
+        var valorLimpo = input.value.replace(/\D/g, "").substring(0, 11);
+        var numerosArray = valorLimpo.split("");
+        var numeroFormatado = ""
+        console.log(numerosArray)
+    
+        if(numerosArray.length === 1){
+            numeroFormatado += `${numerosArray.slice(0,3).join("")}.`;
+        };
+        if(numerosArray.length > 3){
+            numeroFormatado += `${numerosArray.slice(3,6).join("")}.`;
+        };
+        if(numerosArray.length > 6){
+            numeroFormatado += `${numerosArray.slice(6,9).join("")}-`;
+        };
+        if(numerosArray.length > 9){
+            numeroFormatado += `${numerosArray.slice(9,11).join("")}`;
+        };
+    
+        input.value = numeroFormatado
+    };
+    
+    celInput.addEventListener("input", (event) => {
+        maskPhone(celInput);
+    });
+    fixInput.addEventListener("input", (event) => {
+        maskPhone(fixInput);
+    });
+    cpfInput.addEventListener("input", (event) => {
+        maskCPF(cpfInput);
+    });
+    
+    //(01) 2345-6789
