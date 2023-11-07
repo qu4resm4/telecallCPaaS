5f4e8e263467cf36940b08bdccede9328e9eae95
qu4resm4 <quaresmacont@gmail.com>
2023-11-06 21:06:34 -0300

    novoCadastrar.js concluído
    Seguindo a orientação do professor de projeto Charles Bastos esta nova lógica de algoritmo para cadastrar o usuário foi  implementada.
    Adicionando toasts com mensagens para casos especifico de sobreposição de valores como alertado pelo professor.

--- assets/js/novoCadastrar.js
+++ assets/js/novoCadastrar.js
@@ -1,53 +1,58 @@
 //não precisa declarar uma constante selecionando o elemento form, pois já foi feito no validacao.js é carregado antes
+const toast = document.querySelector("#toast1");
+const toast2 = document.querySelector("#toast2")
+const btnTake = document.querySelector(".take");
+const btnClose = document.querySelector("#close1");
+const btnClose2 = document.querySelector("#close2");
 
 // isntanciando valores do localStorage login e senha.
 var localStorage_LoginCadastrado, localStorage_SenhaCadastrada;
 function set(chave, valor) {
-    localStorage.setItem(`${chave}`, valor)
-    return localStorage.getItem(`${chave}`)
+    localStorage.setItem(chave, valor)
+    return localStorage.getItem(chave)
 }
 
 function get(chave){
-    return localStorage.getItem(`${chave}`)
+    return localStorage.getItem(chave)
 }
-localStorage_LoginCadastrado = get("login") === null ? set() : get();
-localStorage_SenhaCadastrada = get("senha") === null ? set() : get();
-
-console.log(localStorage.getItem("login"))
-console.log(localStorage.getItem("senha"))
+localStorage_LoginCadastrado = get("login") === null ? set("login") : get("login");
+localStorage_SenhaCadastrada = get("senha") === null ? set("senha") : get("senha");
 
 // função para verificar se existe valores no localStorage
 function verificandoLocal(login, senha) {
+    console.log("VERIFICANDO O LOCALSOTAGE");
+    let loginLocal = localStorage.getItem("login");
+    let senhaLocal = localStorage.getItem("senha");
 
+    if (loginLocal == "undefined" || senhaLocal == "undefined"){
+        return false
+    } else if (!(loginLocal == login) || !(senhaLocal == senha)) {
+        return true
+    }
     // tem que retorna true se o login ou a senha já estiverem preenchidas
-    return 
 };
 
-// função para exibir toast de sobrepor valores no localStorage
-function recadastrar() {
-    // ALTERAR DISPLAY DO TOAST 
-    
+// ouvindo botões do toast
+btnTake.addEventListener("click", (e) => {
+    localStorage.setItem("login", loginInput.value)
+    localStorage.setItem("senha", passwordInput.value)
+    toast.style.display = "none";
+    toast2.style.display = "block";
+});
+btnClose.addEventListener("click", (e) => {
+    toast.style.display = "none";
+});
 
-    const btnTake = document.querySelector(".take");
-    let valorBtn = btnTake.value;
-
-    return //tem que retornar um valor, se confirmou o recadastro então 
-};
-
+btnClose2.addEventListener("click", (e) => {
+    toast2.style.display = "none";
+});
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault;
+    e.preventDefault();
 
     // tem que verificar se existem valores no local storage,
-    
     if(verificandoLocal(loginInput.value, passwordInput.value)){
-        let valorRecadastrar = recadastrar()
-        if(valorRecadastrar){
-            localStorage.setItem("login", loginInput.value)
-            localStorage.setItem("senha", passwordInput.value)
-        } else {
-
-        }
+        toast.style.display = "block";
     } else {
         localStorage.setItem("login", loginInput.value)
         localStorage.setItem("senha", passwordInput.value)
