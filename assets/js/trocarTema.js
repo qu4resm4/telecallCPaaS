c58c1c5acf95c0c0823f2b3f248f85240b1eab2e
qu4resm4 <quaresmacont@gmail.com>
2023-11-28 17:49:32 -0300

    botão troca de tema funcionadno
    só falta definir melhor as cores do tema escuro

--- assets/js/trocarTema.js
+++ assets/js/trocarTema.js
@@ -1,6 +1,35 @@
 var body = document.querySelector("body");
-var btnTroca = document.querySelector(".tema");
+const btnTroca = document.querySelector("#tema");
+const sun = document.querySelector(".claro");
+const moon = document.querySelector(".escuro");
+var tema;
+function set(chave, valor) {
+    localStorage.setItem(chave, valor)
+    return localStorage.getItem(chave)
+}
 
-btnTroca.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
+function get(chave){
+    return localStorage.getItem(chave)
+}
+tema = get("tema") === null ? set("tema", "light") : get("tema");
+
+if (tema == 'dark') {
+    body.classList.toggle('dark')
+    moon.classList.add('none')
+    sun.classList.remove('none')
+}
+
+btnTroca.addEventListener('click', () => {
+    tema = get("tema")
+    if(tema == 'dark') {
+        localStorage.setItem('tema', 'light')
+        console.log('verificado dark')
+    }
+    if (tema == 'light') {
+        localStorage.setItem('tema', 'dark')
+        console.log('verificado light')
+    }
+    body.classList.toggle('dark')
+    moon.classList.toggle('none')
+    sun.classList.toggle('none')
 })
