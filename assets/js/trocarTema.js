b10a6e18863d7828289fa17e3ff884adcc4ff6a3
qu4resm4 <quaresmacont@gmail.com>
2023-11-29 17:13:04 -0300

    ajuste na funcionalidade de troca de tema

--- assets/js/trocarTema.js
+++ assets/js/trocarTema.js
@@ -14,23 +14,31 @@ function get(chave){
 tema = get("tema") === null ? set("tema", "light") : get("tema");
 
 if (tema == 'dark') {
-    body.classList.toggle('dark')
+    body.classList.add('dark')
     moon.classList.add('none')
     sun.classList.remove('none')
+}
+
+if (tema == 'light') {
+    body.classList.remove('dark')
+    moon.classList.remove('none')
+    sun.classList.add('none')
 }
 
 btnTroca.addEventListener('click', () => {
     tema = get("tema")
     if(tema == 'dark') {
         localStorage.setItem('tema', 'light')
-        console.log('verificado dark')
-        img.classList.remove('none')
+        console.log('verificado dark')            
+        body.classList.remove('dark')
+        moon.classList.remove('none')
+        sun.classList.add('none')
     }
     if (tema == 'light') {
         localStorage.setItem('tema', 'dark')
-        console.log('verificado light')
+        console.log('verificado light')    
+        body.classList.add('dark')
+        moon.classList.add('none')
+        sun.classList.remove('none')
     }
-    body.classList.toggle('dark')
-    moon.classList.toggle('none')
-    sun.classList.toggle('none')
 })
